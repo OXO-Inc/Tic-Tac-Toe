8b7e969829f676c2babba47cbecf976367c46318
Kaushal Agarwal <kaushal.agarwal@oyorooms.com>
2020-01-15 22:38:00 +0530

    Main Menu
    Design & Programming logic

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void playWithComputer()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    
+}
